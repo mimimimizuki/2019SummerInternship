1bccded1b0cc091530510a5b033975c5a96d880a
mizuki <mizuki@mizukinoMacBook-puro.local>
2019-08-19 02:20:47 +0900

    changed database(mySQL -> h2)

--- internship/src/main/java/com/rakuten/internship/DemoApplication.java
+++ internship/src/main/java/com/rakuten/internship/DemoApplication.java
@@ -2,6 +2,9 @@ package com.rakuten.internship;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -9,4 +12,14 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+	@Autowired
+    JdbcTemplate jdbcTemplate;
+    
+    @Override
+    public void run(String... strings) throws Exception {
+        jdbcTemplate.execute("CREATE TABLE todos(" +
+                "id , title VARCHAR(255), description VARCHAR(255))");
+    
+        jdbcTemplate.update("INSERT INTO todos(title, description) VALUES ('Internship HW','make todo lists')");
+    }
 }
